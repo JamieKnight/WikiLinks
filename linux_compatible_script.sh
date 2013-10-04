a026fed154a6fa2b807eabaa2298275f303848ea
jamie <jamie@pluslion.com>
2013-10-04 19:55:29 +0000

    fix checksum generation

--- linux_compatible_script.sh
+++ linux_compatible_script.sh
@@ -10,6 +10,6 @@ function process() {
 	ls data/* | parallel --gnu "grep "bbc.co.uk" {} | grep -o '[^(|=[&;*/ ]*bbc\.co\.uk[^| ]*' > out/$_now/{/}.txt"
 	cat out/$_now/*.txt > results/$_now.results.txt
 	echo "MD5 checksum for results:"
-	echo md5sum results/$_now.results.txt
+	echo $(md5sum results/$_now.results.txt)
 }
 (time process)
