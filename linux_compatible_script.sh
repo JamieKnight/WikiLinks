5b4cb1f94cd6dba6001cec619374b630be1255af
Jamie <jamie@pluslion.com>
2013-10-04 19:24:42 +0000

    Added GNU Linux friendly version of script for cloud benchmarking.

---
+++ linux_compatible_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+_now=$(date +"%m-%d-%y+%s")
+_url=".bbc.co.uk"
+
+echo "Processing input files......"
+mkdir out/$_now
+function process() { 
+	ls data/*.part | parallel --gnu "grep "bbc.co.uk" {} | grep -o '[^(|=[&;*/ ]*bbc\.co\.uk[^| ]*' > out/$_now/{/}.txt"
+}
+(time process)
