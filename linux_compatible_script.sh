58448d3acc5be9c3ebbac67c2a2278d6c58a4c59
jamie <jamie@pluslion.com>
2013-10-04 19:46:42 +0000

    Added checksum functionality

--- linux_compatible_script.sh
+++ linux_compatible_script.sh
@@ -3,8 +3,13 @@ _now=$(date +"%m-%d-%y+%s")
 _url=".bbc.co.uk"
 
 echo "Processing input files......"
+
 mkdir out/$_now
+
 function process() { 
 	ls data/* | parallel --gnu "grep "bbc.co.uk" {} | grep -o '[^(|=[&;*/ ]*bbc\.co\.uk[^| ]*' > out/$_now/{/}.txt"
+	cat out/$_now/*.txt > results/$_now.results.txt
+	echo "MD5 checksum for results:"
+	echo md5sum results/$_now.results.txt
 }
 (time process)
