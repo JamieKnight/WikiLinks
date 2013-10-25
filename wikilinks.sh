3e28dbab6a53907db785cac9720e1592bca45f6d
Jamie Knight <rawr@pluslion.com>
2013-10-25 17:06:09 +0100

    Added varibale, comments and sorting to script.

--- wikilinks.sh
+++ wikilinks.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
+
+#config: 
+
+_url=".bbc.co.uk" #Set this to the URL you wish to find, include a proceding . to capture sub domains
+_escaped="bbc\.co\.uk"
+#End Config
+
 _now=$(date +"%m-%d-%y+%s")
-_url=".bbc.co.uk"
 
 echo "Processing input files......"
 
 mkdir results/$_now
 
 function process() { 
-	ls data/* | parallel --gnu "grep "bbc.co.uk" {} | grep -o '[^(|=[&;*/ ]*bbc\.co\.uk[^| ]*' > results/$_now/{/}.txt"
-	cat results/$_now/*.txt > results/$_now.results.txt
-	echo "MD5 checksum for results:"
-	echo $(md5sum results/$_now.results.txt)
+    #Do the grep, in parallel across all the files passed in.
+	ls data/* | parallel --gnu "grep "$_url" {} | grep -o '[^(|=[&;*/ ]*$_escaped[^| ]*' > results/$_now/{/}.txt"
+	
+	# Merge the results from the parallel stage.
+	cat results/$_now/*.txt > results/$_now/links.txt
+    
+    # Create a checksum of the results for easy result verification during performance tuning.
+    #echo "MD5 checksum for results:"
+    #echo $(md5sum results/$_now.results.txt)
+    
+    # Sort completed, first sort the results, then remove duplicates to get a frequency table.
+    echo "Grep Complete... Sorting and ordering"
+    cat results/$_now/links.txt | sort | uniq -c | sort > results/$_now/unique_links.txt
+    
 }
 (time process)
