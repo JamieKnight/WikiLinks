cb185dd5c234967f97aa6e23a2fa6b78a71fbc55
Jamie Knight <rawr@pluslion.com>
2014-08-03 10:27:07 +0100

    Changes port

--- node/server.js
+++ node/server.js
@@ -57,5 +57,5 @@ http.createServer(function (req, res) {
  res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
 */
-}).listen(1337, '127.0.0.1');
+}).listen(80, '104.131.208.202');
 console.log('Server running at http://127.0.0.1:1337/');
